3ad7fef54f239274b4bca22ac7e926ea3f0a722f
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-18 11:58:19 +0530

    done

--- src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
+++ src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -1,6 +1,10 @@
 package com.epam.rd.autotasks.meetastranger;
+import java.util.Scanner;
 public class MeetAStranger {
     public static void main(String[] args) {
         //Write a program, which read a String from System.in and print "Hello, <input string>"
+        Scanner sc=new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.print("Hello, "+name);
     }
 }
